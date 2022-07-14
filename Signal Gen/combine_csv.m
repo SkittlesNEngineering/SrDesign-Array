58520bd075e352cae00add328482cd9d0bff5a58
SkittlesNEngineering <skittlitz21@gmail.com>
2022-07-13 20:35:23 -0500

    sam: renamevars

--- Signal Gen/combine_csv.m
+++ Signal Gen/combine_csv.m
@@ -2,6 +2,7 @@ function combine_csv(tx1_filename,tx2_filename)
 
 tx1_table = readtable(tx1_filename);
 tx2_table = readtable(tx2_filename);
+tx2_table = renamevars(tx2_table,["Var1","Var2"],["Var3","Var4"])
 combined_table = [tx1_table tx2_table];
 writetable(combined_table, 'leader.csv', 'Delimiter',',')
     
