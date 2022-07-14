d317066ebb9fe2c71aaa8f21920188eb5b5101ea
SkittlesNEngineering <skittlitz21@gmail.com>
2022-07-13 19:53:28 -0500

    sam: sig gen combine function

--- Signal Gen/combine_csv.m
+++ Signal Gen/combine_csv.m
@@ -1,5 +1,7 @@
-tx1_csv = csvread(file1);
-tx2_csv = csvread(file2);
+function combine_csv(tx1_filename,tx2_filename)
+tx1_csv = csvread(tx1_filename);
+tx2_csv = csvread(tx2_filename);
 
 combined = [tx1_csv;tx2_csv]; % Concatenate vertical
-csvwrite(outputfilename, combined)
+csvwrite('leader_zero', combined)
+end
--- Signal Gen/complex_signal_generator.m
+++ Signal Gen/complex_signal_generator.m
@@ -25,8 +25,10 @@ n = seconds * fs;
 f_rad = 1.25e6 * 2 * pi;
 
 % Phase shift, phi or φ, in radians from degrees (φ = °/180 * π)
-deg = 0;
-phi = (deg/180) * pi;
+deg1 = 0;
+phi1 = (deg1/180) * pi;
+deg2 = 0;
+phi2 = (deg2/180) * pi;
 
 % Generate a vector "t" which represents time, in units of samples.
 % This starts at t=0, and creates n samples in steps of 1/SAMPLE_RATE
@@ -34,17 +36,21 @@ t = [ 0 : (1/fs) : (seconds - 1/fs) ];
 
 % Create a sinusoid (signal = e^(jωt) ) with a magnitude of 0.90
 % Shift signal by specified angle (shift = e^(jφ))
-signal = 0.90 * exp(1j * phi) * exp(1j * f_rad * t);
+signal1 = 0.90 * exp(1j * phi1) * exp(1j * f_rad * t);
+signal2 = 0.90 * exp(1j * phi2) * exp(1j * f_rad * t);
 
 % Plot the FFT of our signal as a quick sanity check.
 % The NUM_SAMPLES denominator is just to normalize this for display purposes.
-f = linspace(-0.5 * fs, 0.5 * fs, length(signal));
-plot(f, 20*log10(abs(fftshift(fft(signal)))/n));
-xlabel('Frequency (Hz)');
-ylabel('Power (dB)');
-title('1.25 MHz tone');
+%f = linspace(-0.5 * fs, 0.5 * fs, length(signal));
+%plot(f, 20*log10(abs(fftshift(fft(signal)))/n));
+%xlabel('Frequency (Hz)');
+%ylabel('Power (dB)');
+%title('1.25 MHz tone');
 
 % Save the signal to a file
-save_csv('zero_element1.csv', signal); 
+save_csv('zero_element1.csv', signal1); 
+save_csv('zero_element2.csv', signal2); 
+
+combine_csv('zero_element1.csv','zero_element2.csv')
 
 end
