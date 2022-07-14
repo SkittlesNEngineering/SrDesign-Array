c5a8290ad3f0305672df1d2f9382d0ee0b5a1d92
Melina Stephens <mns130630@utdallas.edu>
2022-07-13 20:00:36 -0500

    melina: test again

--- Signal Gen/combine_csv.m
+++ Signal Gen/combine_csv.m
@@ -3,5 +3,5 @@ tx1_csv = csvread(tx1_filename);
 tx2_csv = csvread(tx2_filename);
 
 combined = [tx1_csv;tx2_csv]; % Concatenate vertical
-csvwrite('leader_zero', combined)
+csvwrite('leader_zero.csv', combined)
 end
