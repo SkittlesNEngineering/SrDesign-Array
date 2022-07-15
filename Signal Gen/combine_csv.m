ec5f1378309392554228019a4158410f9cb6b5f2
Melina Stephens <mns130630@utdallas.edu>
2022-07-14 19:06:19 -0500

    melina: complex sig gen fixed

--- Signal Gen/combine_csv.m
+++ Signal Gen/combine_csv.m
@@ -4,7 +4,7 @@ tx1_table = readtable(tx1_filename);
 tx2_table = readtable(tx2_filename);
 tx2_table = renamevars(tx2_table,["Var1","Var2"],["Var3","Var4"]);
 combined_table = [tx1_table tx2_table];
-combined_table(1,:) = [];
-writetable(combined_table, 'leader_zero.csv', 'Delimiter',',');
+writetable(combined_table, 'leader_zero.csv', 'Delimiter',',','WriteVariableNames',0);
+combined_csv = csvread('leader_zero.csv');
     
 end
--- Signal Gen/complex_signal_generator.m
+++ Signal Gen/complex_signal_generator.m
@@ -22,17 +22,18 @@ seconds = 160e-4;
 n = seconds * fs;
 
 % 1.25 MHz, in radians (ω = F_Hz * 2π)
-f_rad = 1.25e6 * 2 * pi;
+f_hz = 1.25e6;
+f_rad = f_hz * 2 * pi;
 
 % Phase shift, phi or φ, in radians from degrees (φ = °/180 * π)
 deg1 = 0;
 phi1 = (deg1/180) * pi;
-deg2 = 45;
+deg2 = 90;
 phi2 = (deg2/180) * pi;
 
 % Generate a vector "t" which represents time, in units of samples.
 % This starts at t=0, and creates n samples in steps of 1/SAMPLE_RATE
-t = [ 0 : (1/fs) : (seconds - 1/fs) ];
+t = 0 : (1/fs) : (seconds - 1/fs);
 
 % Create a sinusoid (signal = e^(jωt) ) with a magnitude of 0.90
 % Shift signal by specified angle (shift = e^(jφ))
@@ -41,23 +42,39 @@ signal2 = 0.90 * exp(1j * phi2) * exp(1j * f_rad * t);
 
 % Plot the FFT of our signal as a quick sanity check.
 % The NUM_SAMPLES denominator is just to normalize this for display purposes.
-%f = linspace(-0.5 * fs, 0.5 * fs, length(signal));
-%plot(f, 20*log10(abs(fftshift(fft(signal)))/n));
-%xlabel('Frequency (Hz)');
-%ylabel('Power (dB)');
-%title('1.25 MHz tone');
+t_plot = 4/f_hz: 1/fs : (8/f_hz - 1/fs);
+plot(t_plot, signal1(1:96));
+hold on;
+grid on;
+plot(t_plot, signal2(1:96));
+xlabel('Time (s)');
+ylabel('Magnitude');
+
+% f = linspace(-0.5 * fs, 0.5 * fs, length(signal1));
+% plot(f, 20*log10(abs(fftshift(fft(signal1)))/n));
+% grid on;
+% hold on;
+% plot(f, 20*log10(abs(fftshift(fft(signal2)))/n));
+% xlabel('Frequency (Hz)');
+% ylabel('Power (dB)');
+% title('Signals');
 
 % Save the signal to a file
-tx1_filename = 'zero_element1.csv'
-tx2_filename = 'zero_element2.csv'
+tx1_filename = 'zero_element1.csv';
+tx2_filename = 'zero_element2.csv';
+combined_filename = 'leader_zero.csv';
 
 save_csv(tx1_filename, signal1); 
 save_csv(tx2_filename, signal2);
 
-combine_csv(tx1_filename,tx2_filename);
+tx1_table = readtable(tx1_filename);
+tx2_table = readtable(tx2_filename);
+tx2_table = renamevars(tx2_table,["Var1","Var2"],["Var3","Var4"]);
+combined_table = [tx1_table tx2_table];
+writetable(combined_table, combined_filename, 'Delimiter',',','WriteVariableNames',0);
 
-delete tx1_filename;
-delete tx2_filename;
+delete zero_element1.csv;
+delete zero_element2.csv;
     
 
 end
