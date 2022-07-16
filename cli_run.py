99d66b99b010982bce9bf911ad4de0737b6af447
SkittlesNEngineering <skittlitz21@gmail.com>
2022-07-15 19:10:25 -0500

    sam: cli changes, better help in cli

---
+++ cli_run.py
@@ -0,0 +1,60 @@
+import argparse
+from textwrap import dedent
+from subprocess import Popen
+
+less_spacing = lambda prog: argparse.RawTextHelpFormatter(prog,
+                  max_help_position=6)
+
+parser = argparse.ArgumentParser(\
+    formatter_class=less_spacing,
+    description=dedent('''\
+              Run Script Function
+          =+==+==+==+==+==+==+==+==+=
+             Loads text files of
+             commands to the bladeRF
+             SDRs to target the array
+             beam at given angle.
+          ```````````````````````````
+         '''))
+
+# [-h] -ang ANGLE
+# Required argument must be given or function will not run
+parser._action_groups.pop()
+required = parser.add_argument_group('required arguments')
+required.add_argument('angle',choices=[-20,-10,0,10,20], help="Target beam angle in degrees\n\n", type=int)
+
+args = parser.parse_args()
+
+# Run command starts the bladeRF with a precreated txt file of commands.
+# The txt file chosen corresponds to the chosen angle.
+# Target angle must be one of these [-20,-10,0,10,20]
+# If no angle is selected the user will be prompted to try again.
+    # Set str variable angle_txt to string for specific beam angle file
+angle=args.angle
+if angle == -20:
+    angle_txt = "ntwenty"
+elif angle == -10:
+    angle_txt = "nten"
+elif angle == 0:
+    angle_txt = "zero"
+elif angle == 10:
+    angle_txt = "ten"
+elif angle == 20:
+    angle_txt = "twenty"
+
+print("Running bladeRF script files for {} degree beam angle...\n".format(angle))
+
+# Create string for specific beam angle files
+# File for leader and follower must exist in this directory
+leader_file = "leader_" + angle_txt + ".txt"
+follower_file = "follower_" + angle_txt + ".txt"
+
+# Open leader and follower command prompts and execute bladeRF command to load selected files
+# Follower SDR will wait for trigger from leader command prompt
+# Command prompt opener string is specific for Microsoft OS
+process1 = Popen(['start', '/wait', 'cmd', '/k', "bladeRF-cli -s " + leader_file + " -i"], shell = True)
+process2 = Popen(['start', '/wait', 'cmd', '/k', "bladeRF-cli -s " + follower_file + " -i"], shell = True)
+process1.wait()
+process2.wait()
+
+print("")
--- sdr_cli.py
+++ sdr_cli.py
@@ -1,4 +1,3 @@
-from subprocess import Popen
 from os import system
 
 def run_sdr_command(usr_input_str: str):
@@ -7,45 +6,26 @@ def run_sdr_command(usr_input_str: str):
     usr_command = usr_input_list[0].lower()
 
     if usr_command=="run" or usr_command=="r":
-        # Run command starts the bladeRF with a precreated txt file of commands.
-        # The txt file chosen corresponds to the chosen angle.
-        # Target angle must be one of these [-20,-10,0,10,20]
-        # If no angle is selected the user will be prompted to try again.
-        angle=int(usr_input_list[1])
-        if angle == -20:
-            # system("bladeRF-cli -s signal_ntwenty.txt -i")
-            angle_txt = "ntwenty"
-        elif angle == -10:
-            # system("bladeRF-cli -s signal_nten.txt -i")
-            angle_txt = "nten"
-        elif angle == 0:
-            # system("bladeRF-cli -s signal_zero.txt -i")
-            angle_txt = "zero"
-        elif angle == 10:
-            # system("bladeRF-cli -s signal_ten.txt -i")
-            angle_txt = "ten"
-        elif angle == 20:
-            # system("bladeRF-cli -s signal_twenty.txt -i")
-            angle_txt = "twenty"
-        else: return "ERR: Target angle is not in set. \nTry again with one of the following angles: [-20,-10,0,10,20]"
-    
-        
-        leader_file = "leader_" + angle_txt + ".txt"
-        follower_file = "follower_" + angle_txt + ".txt"
-        process1 = Popen(['start', '/wait', 'cmd', '/k', "bladeRF-cli -s " + leader_file + " -i"], shell = True)
-        process2 = Popen(['start', '/wait', 'cmd', '/k', "bladeRF-cli -s " + follower_file + " -i"], shell = True)
-        process1.wait()
-        process2.wait()
+    # DESCRIPTION
+        arg_str = (' '.join(usr_input_list[1::]))
+        system("python3 cli_run.py "+arg_str)
         pass
 
     elif usr_command=="interactive" or usr_command=="i":
-        print("Entering Interactive Mode...")
-        system("bladeRF-cli -i")
+    # DESCRIPTION
+        arg_str = (' '.join(usr_input_list[1::]))
+        system("python3 cli_interactive.py "+arg_str)
         pass
 
     elif usr_command=="probe" or usr_command=="p":
-        print("Probing for bladeRF devices...")
-        system("bladeRF-cli -p")
+    # DESCRIPTION
+        arg_str = (' '.join(usr_input_list[1::]))
+        system("python3 cli_probe.py "+arg_str)
+        pass
+
+    elif usr_command=="help" or usr_command=="h":
+    # Help command provides list of allowed commands and needed params
+        system("python3 cli_help.py")
         pass
 
     elif usr_command=="quit" or usr_command=="q":
@@ -66,9 +46,7 @@ def run_sdr_command(usr_input_str: str):
 def main():
     print("Welcome to the SDR Controller... ")
     while True:
-        rtrn_str = run_sdr_command(input("controller@bladeRF:~$ "))
-        if type(rtrn_str)==str:
-            print(rtrn_str)
+        run_sdr_command(input("controller@bladeRF:~$ "))
 
 if __name__ == '__main__':
     main()
